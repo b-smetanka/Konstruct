e4bdc9c38b74fb5992cbdfdfd0ac35d3a88ca4f1
b.smetanka <w.smetanka@gmail.com>
2020-10-31 16:30:54 +0300

    new

--- src/js/components/main-slider.js
+++ src/js/components/main-slider.js
@@ -1,25 +1,28 @@
 import Swiper from "../vendor/swiper.min.js";
-import vars from "../_vars";
+import vars from '../_vars';
 
 const bannerSlider = new Swiper(vars.$bannerSlider, {
-   loop: true,
-   containerModifierClass: 'banner-slider-',
-   slidesPerView: 1,
-   navigation: {
-      nextEl: '.box-prev-btn',
-      prevEl: '.box-next-btn',
-   },
-   autoplay: {
-      delay: 3000,
-   },
-   breakpoints: {
-      // when window width is >= 480px
-      480: {
-         slidesPerView: 1,
-      },
-      // when window width is >= 640px
-      640: {
-         slidesPerView: 2,
-      }
-   }
+  loop: true,
+  containerModifierClass: 'banner-slider-',
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.box-prev-btn',
+    prevEl: '.box-next-btn',
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 880px
+    880: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 940px
+    940: {
+      slidesPerView: 3,
+    }
+  }
 });
